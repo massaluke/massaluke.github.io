a70ba935a86963f4ddb727eb5d9c70743273726b
massaluke <lukemassa2003@gmail.com>
2024-08-19 15:20:22 +1000

    trying to manyally turn button green

--- netlify/functions/getStatus.js
+++ netlify/functions/getStatus.js
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
     }
     return {
       statusCode: 200,
-      body: JSON.stringify({ status: 'redrub' }), // Default status
+      body: JSON.stringify({ status: 'green' }), // Default status
     };
   } catch (error) {
     return {
